2e56fa4d3eaec290c5f89a5094e2d8b233881b38
rahmi45 <rachid.rahmi@gmail.com>
2022-03-07 19:37:23 +0100

    Projet SOAP BanqueWS

---
+++ src/serveur/ServeurJaxWS.java
@@ -0,0 +1,13 @@
+package serveur;
+
+import ws.BanqueService;
+
+import javax.xml.ws.Endpoint;
+
+public class ServeurJaxWS {
+    public static void main(String[] args) {
+        String url = "http://0.0.0.0:8686/";
+        Endpoint.publish(url, new BanqueService());
+        System.out.println("Web service déployé sur " + url);
+    }
+}
